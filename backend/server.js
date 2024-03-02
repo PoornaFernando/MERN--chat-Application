ae56c48c5fec50edd9d3032ea98088bc37ffece0
PoornaFernando <poornafdo98@gmail.com>
2024-03-02 22:41:19 +0530

    nodepath

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
-const path = require('path')
+const path = require('node:path');
 
 const databaseConnect = require('./config/database')
 const authRouter = require('./routes/authRoute')
