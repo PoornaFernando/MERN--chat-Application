7f25971263e884430f5bbdd9c78deefc0b81aee1
PoornaFernando <poornafdo98@gmail.com>
2024-03-09 17:09:40 +0530

    docker

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use('/api/messenger', messengerRoute);
 
 const PORT = process.env.PORT || 5000
 app.get('/', (req, res)=> {
-    res.send('This is from backend')
+    res.send('This is from backend - Chat App Group 01 DSCC')
 })
 
 databaseConnect();
